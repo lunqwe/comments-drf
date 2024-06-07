e46e5cbc8d29fe8521026989b86a1348d6fae974
lunqwe <roma.boyko10068@gmail.com>
2024-06-08 02:39:31 +0300

    create comment endpoint

--- comments/consumers.py
+++ comments/consumers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from asgiref.sync import sync_to_async
+import json
 from channels.db import database_sync_to_async
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
 from djangochannelsrestframework import mixins
@@ -7,7 +8,6 @@ from djangochannelsrestframework.observer.generics import (ObserverModelInstance
 from djangochannelsrestframework.observer import model_observer
 from django.core.paginator import Paginator
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Comment
@@ -20,13 +20,47 @@ class WebsocketConsumer(ObserverModelInstanceMixin, GenericAsyncAPIConsumer):
     authentication_classes = (JWTAuthentication, )
     
     async def connect(self):
+        await self.channel_layer.group_add(
+            'comments_group',
+            self.channel_name
+        )
         await self.accept()
+        
+    @sync_to_async()
+    def load_comments(self, page_num, **kwargs):
+        queryset = Comment.objects.all()
+        paginator = Paginator(queryset, 50)
+        result = paginator.get_page(page_num)
+        comments = []
+        for comment in result:
+            comments.append({
+                'user_name': comment.user_name,
+                'email': comment.email,
+                'text': comment.text
+            })
+        return comments
     
     @action()
-    async def add_comment(self, message, **kwargs):
-        await self.send_json(message) 
+    async def get_comments(self, page_num, **kwargs):
+        comments = await self.load_comments(page_num=page_num)
+        return await self.send(text_data=json.dumps({
+            'event_type': 'display_comment',
+            'comment': comments
+        }))
+        
+    
+    async def add_comment(self, event):
+        comment = event['comment']
+        await self.send(text_data=json.dumps({
+            'event_type': 'display_comment',
+            'comment': comment
+        }))
             
     @action()
     async def logout(self, **kwargs):
+        await self.channel_layer.group_discard(
+            'comments_group',
+            self.channel_name
+        )
         await self.close(1000)
     
--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,41 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+
+from .models import Comment
+from .serializers import CommentSerializer
 
 def index(request):
     return render(request, 'index.html')
+
+
+class AddCommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    authentication_classes = (JWTAuthentication, )
+        
+    def perform_create(self, serializer):
+        comment = serializer.save()
+        comment_data = CommentSerializer(comment).data
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            'comments_group',
+            {
+                'type': 'add_comment',
+                'comment': comment_data
+            }
+        )
+        
+        
+
+        
+
+
+
+    
+    
+    
