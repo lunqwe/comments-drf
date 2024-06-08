10272dffa4ae029f3d74470e886be7836c78e6ad
lunqwe <roma.boyko10068@gmail.com>
2024-06-08 03:13:42 +0300

    refactoring / updated comment model

--- comments/models.py
+++ comments/models.py
@@ -1,34 +1,8 @@
 from django.db import models
-from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
-
-class UserManager(BaseUserManager):
-    
-    def create_user(self, email: str, username: str, password: str, **extra_fields):
-        if not email:
-            raise ValueError('Email must be set.')
-        
-        normalized_email = self.normalize_email(email=email)
-        user = self.model(email=normalized_email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        
-        return user
-
-    
-    def create_superuser(self, email: str, username: str, password: str, **extra_fields):
-        extra_fields.setdefault(is_staff=True)
-        extra_fields.setdefault(is_superuser=True)
-        
-        return User.objects.create_user(email=email, username=username, password=password, **extra_fields)
-    
-
-class User(AbstractUser):
-    username = models.CharField("Username", max_length=255, unique=True)
-    email = models.EmailField('Email', unique=True)
-    
 
 class Comment(models.Model):
-    text = models.CharField('self', max_length=255)
+    created_by = models.CharField("Username", max_length=255, default='Guest')
+    email = models.EmailField('Email', default='')
+    text = models.TextField('Text')
+    created_at = models.DateTimeField(auto_now_add=True)
