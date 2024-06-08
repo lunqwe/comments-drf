7a4c53accba317f9a05f1d1aafe1efa263f2dd61
lunqwe <roma.boyko10068@gmail.com>
2024-06-08 03:17:17 +0300

    routing & refactoring

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from .views import index, AddCommentView
+from rest_framework.routers import DefaultRouter
+
+
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('comments/create/', AddCommentView.as_view(), name='create-comment')  
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, mixins
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
 from channels.layers import get_channel_layer
@@ -13,13 +13,20 @@ def index(request):
     return render(request, 'index.html')
 
 
-class AddCommentViewSet(viewsets.ModelViewSet):
+class AddCommentView(generics.CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     authentication_classes = (JWTAuthentication, )
         
     def perform_create(self, serializer):
         comment = serializer.save()
+        user = self.request.user
+
+        if user.is_authenticated:
+            comment.created_by = user.created_by
+            comment.email = user.email
+            comment.save()
+            
         comment_data = CommentSerializer(comment).data
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
@@ -30,7 +37,7 @@ class AddCommentViewSet(viewsets.ModelViewSet):
             }
         )
         
-        
+    
 
         
 
