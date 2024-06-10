16877807f6db48a5e8bf0b4ceff92deab869877f
lunqwe <roma.boyko10068@gmail.com>
2024-06-11 01:22:37 +0300

    fixed requirement for endpoint

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    to_comment_id = serializers.IntegerField()
+    to_comment_id = serializers.IntegerField(required=False)
     class Meta:
         model = Comment
         fields = ['id', 'created_by_username', 'email', 'text', 'owner', 'to_comment_id']
