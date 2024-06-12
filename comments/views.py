715f23c1b95418bfea940646038d99c614c36157
lunqwe <roma.boyko10068@gmail.com>
2024-06-12 23:15:46 +0300

    fgdgdgdg

--- comments/views.py
+++ comments/views.py
@@ -39,6 +39,14 @@ class AddCommentView(generics.CreateAPIView):
                 
         serializer.data['file'] == comment.file.url
         
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        print(serializer.data)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        
         
 # get comments view
 class CommentListView(generics.ListAPIView):
